a469fe2c0fc97f9aa460c22c5bdd68553645d077
Ziyang <543559992@qq.com>
2016-07-27 11:32:55 +0800

    修改BUG

--- app/src/main/java/com/android/loushi/loushi/util/MyApplication.java
+++ app/src/main/java/com/android/loushi/loushi/util/MyApplication.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.android.loushi.loushi.callback.JsonCallback;
 import com.android.loushi.loushi.jsonbean.ResponseJson;
 import com.android.loushi.loushi.jsonbean.UserLoginJson;
+import com.android.loushi.loushi.ui.activity.BaseActivity;
 import com.lzy.okhttputils.OkHttpUtils;
 import com.lzy.okhttputils.cookie.store.PersistentCookieStore;
 
@@ -34,7 +35,7 @@ public class MyApplication extends LitePalApplication {
         OkHttpUtils.getInstance()//
                 .debug("OkHttpUtils").setCookieStore(new PersistentCookieStore());
 
-        OkHttpUtils.post("http://119.29.187.58:10000/LouShi/user/userLogin.action")
+        OkHttpUtils.post(BaseActivity.url+"/LouShi/user/userLogin.action")
                 // 请求方式和请求url
                 .tag(this).params("mobile_phone", "13750065622").params("password", "mtf071330")
                 .params("isThird", "false")
