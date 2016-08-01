012d84f07377c482e31e5a1571b4d464fd6f1ed0
matf5 <419194613@qq.com>
2016-08-01 09:52:35 +0800

    gai

---
+++ app/src/main/java/com/android/loushi/loushi/ui/activity/FeedActivity.java
@@ -0,0 +1,12 @@
+package com.android.loushi.loushi.ui.activity;
+
+/**
+ * Created by Administrator on 2016/7/31.
+ */
+public class FeedActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+}
